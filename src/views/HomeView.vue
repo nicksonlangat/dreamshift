91636466c41d99234b798dc26beb6b5a239714fc
nicksonlangat <nicksonlangat95@gmail.com>
2024-02-17 14:17:15 +0300

    initial commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,247 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<main>
+    <div class="bg-[#072a0c] pb-10">
+        <div class="mx-auto container">
+            <div class="flex mx-5 lg:mx-0 justify-between items-center">
+                <h1 class="text-[#c1ff6e] mt-5 text-2xl lg:text-4xl font-extrabold">DreamShift</h1>
+                <div class="mt-5 hidden  text-white lg:flex gap-5 ">
+                    <a href="#services">Services</a>
+                    <a href="#how">How It Works</a>
+                    <a href="#testimonials">Testimonials</a>
+                    <a href="#faq">FAQ</a>
+                </div>
+                <div class="mt-5">
+                    <a href="#contact" class="lg:px-6 lg:py-2 text-xs font-extralight lg:text-base lg:font-normal py-1 px-3 border border-[#c1ff6e] rounded-full mt-5 text-white">Get a free estimate</a>
+                </div>
+            </div>
+            <div class="hidden lg:flex items-center justify-center gap-10 p-10 mt-10">
+                <div class="text-white flex flex-col items-start gap-3 justify-center">
+                    <div class="flex text-amber-500">
+                        <svg v-for="i in 5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5">
+                            <path fill-rule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z" clip-rule="evenodd" />
+                        </svg>
+
+                    </div>
+                    <p>"I will always use their services"</p>
+                </div>
+                <div class="text-white flex flex-col items-start gap-3 justify-center">
+                    <div class="flex text-amber-500">
+                        <svg v-for="i in 5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5">
+                            <path fill-rule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z" clip-rule="evenodd" />
+                        </svg>
+
+                    </div>
+                    <p>"I will always use their services"</p>
+                </div>
+                <div class="text-white flex flex-col items-start gap-3 justify-center">
+                    <div class="flex text-amber-500">
+                        <svg v-for="i in 5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-5 h-5">
+                            <path fill-rule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z" clip-rule="evenodd" />
+                        </svg>
+
+                    </div>
+                    <p>"I will always use their services"</p>
+                </div>
+            </div>
+
+            <div class="flex mt-10 mx-5 lg:mx-0 lg:mt-0 items-center justify-center ">
+                <div class="w-full max-w-4xl">
+                    <h1 class="lg:text-5xl text-start lg:text-center text-3xl text-white leading-32 font-bold">Kenya's <span class="text-[#c1ff6e]">top choice</span> for premium and affordable <span class="text-[#c1ff6e]">house mover</span> services</h1>
+
+                </div>
+
+            </div>
+            <div class="mt-10 hidden lg:block">
+                <p class="text-white"><span class="text-[#c1ff6e]">Before DreamShift:</span> Painful struggles when moving and item breakages.</p>
+                <p class="text-white"><span class="text-[#c1ff6e]">After DreamShift:</span> Safe, Fast & Effortless evacuation from one place to another.</p>
+            </div>
+
+           <div class="mt-10">
+            <a href="#contact" class="bg-[#c1ff6e] text-[#072a0c] py-2 rounded-full px-6  font-bold">Get a free estimate</a>
+           </div>
+            <p class="mt-5 text-white">10,000+ Happy Kenyans moved and counting!</p>
+        </div>
+
+    </div>
+    <div class="bg-white">
+        <div id="services" class="mx-auto container p-5">
+            <h3 class=" text-3xl font-bold">Services</h3>
+            <div class=" grid lg:grid-cols-3 gap-5 mt-5">
+                <img class="rounded-lg" src="https://plus.unsplash.com/premium_photo-1663045627496-441affafddf1" alt="">
+                <img class="rounded-lg" src="https://images.unsplash.com/photo-1614359835514-92f8ba196357" alt="">
+                <img class="rounded-lg" src="https://plus.unsplash.com/premium_photo-1661907153090-93759d68acb1" alt="">
+            </div>
+        </div>
+    </div>
+    <div id="how" class="bg-[#faf7f0] lg:h-80 pb-4 lg:pb-0 mt-2 flex flex-col items-center justify-center">
+
+        <h1 class="text-[#020101] mt-5 lg:mt-0 lg:text-4xl text-2xl font-bold">Getting started is easy</h1>
+        <div  class="grid text-[#072a0c] mx-5 lg:mx-44 text-start lg:grid-cols-3 gap-5 mt-5">
+            <div class="flex flex-col gap-5">
+                <div>
+                    <span class="h-10 w-10 flex items-center justify-center rounded-full border border-[#072a0c]">
+                        1
+                    </span>
+                </div>
+                <div>
+                    <h3 class="text-xl text-start font-bold">Submit Request</h3>
+                    <p>Request a free cost estimate online for any of our services.</p>
+                </div>
+            </div>
+            <div class="flex flex-col gap-5">
+                <div>
+                    <span class="h-10 w-10 flex items-center justify-center rounded-full border border-[#072a0c]">
+                        2
+                    </span>
+                </div>
+                <div>
+                    <h3 class="text-xl text-start font-bold">Schedule Visit</h3>
+                    <p>Pick a time for us to do the work. We'll confirm and send a reminder.</p>
+                </div>
+            </div>
+            <div class="flex flex-col gap-5">
+                <div>
+                    <span class="h-10 w-10 flex items-center justify-center rounded-full border border-[#072a0c]">
+                        3
+                    </span>
+                </div>
+                <div>
+                    <h3 class="text-xl text-start font-bold">Enjoy Your New Home</h3>
+                    <p>Sit back while we ensure your items are moved to your new place safely.</p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <div id="testimonials" class="mx-auto mt-5 container">
+        <h1 class="text-3xl font-bold">Customer Testimonials</h1>
+        <div class="grid p-5 lg:grid-cols-3 gap-5">
+            <div>
+                <img class="rounded-t-xl" src="https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91" alt="">
+                <div class="border rounded-b-xl">
+                    <p class="text-start p-5 ">Exceptional service! The team at DreamShift made our relocation seamless. Their efficiency & care for our belongings exceeded our expectations. From packing to delivery, every detail was handled with precision. DreamShift is the go-to house mover for a stress-free and reliable moving experience. Highly recommend!</p>
+                </div>
+            </div>
+            <div>
+                <img class="rounded-t-xl" src="https://images.unsplash.com/photo-1568602471122-7832951cc4c5" alt="">
+                <div class="border rounded-b-xl">
+                    <p class="text-start p-5 ">
+                        Relocating with DreamShift was a breeze! The team was punctual, diligent, and treated our belongings with the utmost care. From start to finish, their attention to detail and friendly service made our move stress-free. DreamShift is the epitome of reliability, making them our top choice for any future moves. Highly recommended!
+                    </p>
+                </div>
+            </div>
+            <div>
+                <img class="rounded-t-xl" src="https://images.unsplash.com/photo-1619895862022-09114b41f16f" alt="">
+                <div class="border rounded-b-xl">
+                    <p class="text-start p-5 ">
+                        Choosing DreamShift for our move was the best decision! The team exhibited professionalism and efficiency throughout the entire process. From meticulous packing to timely delivery, every step was handled seamlessly. Their commitment to customer satisfaction and the safety of our belongings sets them apart. A heartfelt thank you!
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="faq" class="mt-5 bg-white mx-auto container">
+        <h1 class="lg:text-3xl text-2xl font-bold">Frequently asked questions</h1>
+        <div class="lg:mt-5 p-5 flex flex-col lg:gap-5">
+            <div class="grid lg:grid-cols-2 lg:gap-10 gap-5">
+                <div v-for="faq in faqs" class="bg-[#faf7f0] rounded-2xl">
+                    <div class="p-5">
+                        <div class="flex justify-between">
+                            <h3 class="text-lg font-bold">{{ faq.title }}</h3>
+                            <svg v-if="!faq.isOpen" @click="faq.isOpen=true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 cursor-pointer h-6">
+                                <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 9a.75.75 0 0 0-1.5 0v2.25H9a.75.75 0 0 0 0 1.5h2.25V15a.75.75 0 0 0 1.5 0v-2.25H15a.75.75 0 0 0 0-1.5h-2.25V9Z" clip-rule="evenodd" />
+                            </svg>
+                            <svg v-if="faq.isOpen" @click="faq.isOpen=false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 cursor-pointer h-6">
+                                <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm-1.72 6.97a.75.75 0 1 0-1.06 1.06L10.94 12l-1.72 1.72a.75.75 0 1 0 1.06 1.06L12 13.06l1.72 1.72a.75.75 0 1 0 1.06-1.06L13.06 12l1.72-1.72a.75.75 0 1 0-1.06-1.06L12 10.94l-1.72-1.72Z" clip-rule="evenodd" />
+                            </svg>
+
+                        </div>
+                        <p v-if="faq.isOpen" class="text-start mt-4 py-2">{{ faq.content }}</p>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+    <div class="bg-[#072a0c] mt-5 pb-2">
+        <div class="mx-auto container">
+
+            <div class="p-3">
+                <form id="contact" class="w-full max-w-4xl text-start text-white">
+                    <h2 class="text-2xl text-[#c1ff6e] mt-10 font-bold text-start">Get your free estimate today</h2>
+                    <div class="grid lg:grid-cols-2 mt-10 gap-10 gap-y-6">
+                        <div class="flex flex-col gap-3">
+                            <label for="">Name<span class="text-red-600">*</span> </label>
+                            <input type="text" class="border-b bg-inherit focus:outline-none focus:ring-0 border-white/10 placeholder:text-white/30" placeholder="Your name">
+                        </div>
+                        <div class="flex flex-col gap-3">
+                            <label for="">Phone number<span class="text-red-600">*</span> </label>
+                            <input type="text" class="border-b bg-inherit focus:outline-none focus:ring-0 border-white/10 placeholder:text-white/30" placeholder="Your phone number">
+                        </div>
+                        <div class="flex flex-col gap-3">
+                            <label for="Name">Email<span class="text-red-600">*</span> </label>
+                            <input type="email" class="border-b bg-inherit focus:outline-none focus:ring-0 border-white/10 placeholder:text-white/30" placeholder="Your email">
+                        </div>
+                        <div class="flex flex-col gap-3">
+                            <label for="">Address<span class="text-red-600">*</span> </label>
+                            <input type="text" class="border-b bg-inherit focus:outline-none focus:ring-0 border-white/10 placeholder:text-white/30" placeholder="Your current location">
+                        </div>
+                        <div class="flex flex-col gap-3">
+                            <label for="">Destination<span class="text-red-600">*</span> </label>
+                            <input type="text" class="border-b bg-inherit focus:outline-none focus:ring-0 border-white/10 placeholder:text-white/30" placeholder="Your new location">
+                        </div>
+
+                    </div>
+                    <button @click.prevent="submit" class="bg-[#c1ff6e] text-[#072a0c] px-4 py-2 mt-10 rounded-full">Get a free estimate</button>
+                </form>
+            </div>
+            <div class="hidden lg:flex p-3 justify-between text-white items-center">
+                <h1 class="text-[#c1ff6e] text-xl font-extrabold">DreamShift</h1>
+                <div class="hidden lg:flex gap-5 items-center ">
+                  <a href="#services">Services</a>
+                    <a href="#how">How It Works</a>
+                    <a href="#testimonials">Testimonials</a>
+                    <a href="#faq">FAQ</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+
+    components: {
+
+    },
+    data() {
+        return {
+            show: false,
+            faqs: [{
+                    "title": "Why DreamShift?",
+                    "content": "We promise professionalism, efficiency, and care for your belongings, from packing to delivery.",
+                    "isOpen": false
+                },
+                {
+                    "title": "What kind of guarantee do you offer?",
+                    "content": "We promise professionalism, efficiency, and care for your belongings, from packing to delivery.",
+                    "isOpen": false
+                },
+                {
+                    "title": "Do you offer any discounts or promotions?",
+                    "content": "Yes, from time to time we offer great offers tailored just for you!",
+                    "isOpen": false
+                },
+                {
+                    "title": "What kind of licensing or insurance do you have?",
+                    "content": "We have full comprehensive insurance to protect against unforeseen disasters.",
+                    "isOpen": false
+                }
+            ]
+        }
+    }
 }
 </script>
